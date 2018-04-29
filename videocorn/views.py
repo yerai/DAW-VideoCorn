34212bb53cdbea36828120c2ba8187dd0fbd0ad9
Yerai <yeerizg@gmail.com>
2018-04-29 19:31:11 +0200

    Vista home hasta los links

--- videocorn/views.py
+++ videocorn/views.py
@@ -1,36 +1,41 @@
 from django.shortcuts import render
 from .models import Genre, Year, Score, Actor, Movie
 from django.contrib.auth.decorators import login_required
-from .forms import SearchForm
 
 @login_required
 def home(request):
 
     #Obtenemos de la BD
     queryset_list = Movie.objects.all()
+    year_list = Year.objects.all().order_by('date')
+    genre_list = Genre.objects.all().order_by('name')
+    score_list = Score.objects.all().order_by('stars')
 
     #Obtenemos los parametros de busqueda
     title = request.GET.get("title")
+    year = request.GET.get("year")
     genre = request.GET.get("genre")
     score = request.GET.get("score")
-    year = request.GET.get("year")
-
-    year_list = list(Year.objects.all())
-    hola = print(year)
-    hola2 = string(hola)
 
     #Filtramos
     if title:
         queryset_list = queryset_list.filter(title__icontains=title)
-    
-    
-    # Form
-    form = SearchForm(request.GET)
+    if year:
+        queryset_list = queryset_list.filter(year__date=year)
+    if genre:
+        queryset_list = queryset_list.filter(genre__name=genre)
+    if score:
+        queryset_list = queryset_list.filter(score__stars=score)
 
     # Context
     context = {
         "movie_list" : queryset_list,
-        "form" : form,
+        "year_list" : year_list,
+        "genre_list" : genre_list,
+        "score_list" : score_list,
+        "year" : year,
+        "genre" : genre,
+        "score" : score,
     }
     # Render the HTML template home.html with the data in the context variable
     return render(
